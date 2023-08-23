65c15a2b2827c25ed7c459bce11e602fa7c0ebc1
arsscriptum <guillaumeplante.qc@gmail.com>
2023-08-23 15:32:13 -0400

    auto

---
+++ Convert-OutputForCSV/Convert-OutputForCSV.ps1
@@ -0,0 +1,85 @@
+
+<#
+#̷𝓍   𝓐𝓡𝓢 𝓢𝓒𝓡𝓘𝓟𝓣𝓤𝓜
+#̷𝓍   🇵​​​​​🇴​​​​​🇼​​​​​🇪​​​​​🇷​​​​​🇸​​​​​🇭​​​​​🇪​​​​​🇱​​​​​🇱​​​​​ 🇸​​​​​🇨​​​​​🇷​​​​​🇮​​​​​🇵​​​​​🇹​​​​​ 🇧​​​​​🇾​​​​​ 🇬​​​​​🇺​​​​​🇮​​​​​🇱​​​​​🇱​​​​​🇦​​​​​🇺​​​​​🇲​​​​​🇪​​​​​🇵​​​​​🇱​​​​​🇦​​​​​🇳​​​​​🇹​​​​​🇪​​​​​.🇶​​​​​🇨​​​​​@🇬​​​​​🇲​​​​​🇦​​​​​🇮​​​​​🇱​​​​​.🇨​​​​​🇴​​​​​🇲​​​​​
+#>
+
+
+﻿Function Convert-OutputForCSV {
+    <#
+        .SYNOPSIS
+            Provides a way to expand collections in an object property prior
+            to being sent to Export-Csv.
+
+        .DESCRIPTION
+            Provides a way to expand collections in an object property prior
+            to being sent to Export-Csv. This helps to avoid the object type
+            from being shown such as system.object[] in a spreadsheet.
+
+        .PARAMETER InputObject
+            The object that will be sent to Export-Csv
+
+        .PARAMETER OutPropertyType
+            This determines whether the property that has the collection will be
+            shown in the CSV as a comma delimmited string or as a stacked string.
+
+            Possible values:
+            Stack
+            Comma
+
+            Default value is: Stack        
+            
+    #>
+
+    [cmdletbinding(SupportsShouldProcess)]
+    Param (
+        [parameter(ValueFromPipeline)]
+        [psobject]$InputObject,
+        [parameter()]
+        [ValidateSet('Stack','Comma')]
+        [string]$OutputPropertyType = 'Stack'
+    )
+    Begin {
+        $PSBoundParameters.GetEnumerator() | ForEach {
+            Write-Verbose "$($_)"
+        }
+        $FirstRun = $True
+    }
+    Process {
+        If ($FirstRun) {
+            $OutputOrder = $InputObject.psobject.properties.name
+            Write-Verbose "Output Order:`n $($OutputOrder -join ', ' )"
+            $FirstRun = $False
+            #Get properties to process
+            $Properties = Get-Member -InputObject $InputObject -MemberType *Property
+            #Get properties that hold a collection
+            $Properties_Collection = @(($Properties | Where-Object {
+                $_.Definition -match "Collection|\[\]"
+            }).Name)
+            #Get properties that do not hold a collection
+            $Properties_NoCollection = @(($Properties | Where-Object {
+                $_.Definition -notmatch "Collection|\[\]"
+            }).Name)
+            Write-Verbose "Properties Found that have collections:`n $(($Properties_Collection) -join ', ')"
+            Write-Verbose "Properties Found that have no collections:`n $(($Properties_NoCollection) -join ', ')"
+        }
+ 
+        $InputObject | ForEach {
+            $Line = $_
+            $stringBuilder = New-Object Text.StringBuilder
+            $Null = $stringBuilder.AppendLine("[pscustomobject] @{")
+
+            $OutputOrder | ForEach {
+                If ($OutputPropertyType -eq 'Stack') {
+                    $Null = $stringBuilder.AppendLine("`"$($_)`" = `"$(($line.$($_) | Out-String).Trim())`"")
+                } ElseIf ($OutputPropertyType -eq "Comma") {
+                    $Null = $stringBuilder.AppendLine("`"$($_)`" = `"$($line.$($_) -join ', ')`"")                   
+                }
+            }
+            $Null = $stringBuilder.AppendLine("}")
+ 
+            Invoke-Expression $stringBuilder.ToString()
+        }
+    }
+    End {}
+}
