f21ed33e0e96cc9068a9abb1623deb56994c5131
arsscriptum <guillaumeplante.qc@gmail.com>
2023-09-22 21:06:34 -0400

    auto

--- AutoUpdateScript/AutoUpdate.ps1
+++ AutoUpdateScript/AutoUpdate.ps1
@@ -4,7 +4,6 @@
 #>
 
 
-$new = 0
 function Invoke-AutoUpdate{
     [CmdletBinding(SupportsShouldProcess)]
     param() 
@@ -22,15 +21,16 @@ function Invoke-AutoUpdate{
     }
     process{
       try{
-         & "$GitExe" 'fetch'
+        $Output = & "$GitExe" 'fetch' | Out-Null
+        $HeadRev = & "$GitExe"  'log' '-n' '1' '--no-decorate' '--pretty=format:%H'  "$ScriptPath"
+        
         [uint32]$NewVers = & "$GitExe" 'diff' 'remotes/origin/master..master'  "$ScriptPath" | Measure-Object -Line | Select -ExpandProperty Lines
         if($NewVers -gt 0){
             Write-Host "A new version is available for `"$ScriptPath`"" -f Cyan
-
-            $HeadRev = & "$GitExe"  'log' '-n' '1' '--no-decorate' '--pretty=format:%H'  "$ScriptPath"
-            Write-Host "Head Rev: `"$HeadRev`""
+            Write-Host "Head Rev: `"$HeadRev`"" -f Yellow
         }else{
              Write-Host "No updates for `"$ScriptPath`"" -f Yellow
+             Write-Host "Head Rev: `"$HeadRev`"" -f Yellow
         }
 
 
